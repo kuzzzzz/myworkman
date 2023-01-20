3c4b2c623af0f47ef98ad347c990edfd90ac26a9
kuzzzzz <atumahdaniel@gmail.com>
2023-01-20 19:43:53 +0100

    new episode  iframe upload

--- pages/shows/index.js
+++ pages/shows/index.js
@@ -19,8 +19,15 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>Shows</h1>
       <iframe
+        src="https://anchor.fm/myworkman/embed/episodes/My-Work-Man-Show-Ep-II-e1tp43t/a-a975gj0"
+        height="120px"
+        width="500px"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+      <iframe
         src="https://anchor.fm/myworkman/embed/episodes/My-work-man-show-ep-one-e1tet6t"
-        height="202px"
+        height="120px"
         width="500px"
         frameBorder="0"
         scrolling="no"
