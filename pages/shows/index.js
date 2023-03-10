44ccd2b3c5a2a09e5a07948d86d3ba8f401781d6
kuzzzzz <atumahdaniel@gmail.com>
2023-03-10 21:14:52 +0100

    weekly episode update

--- pages/shows/index.js
+++ pages/shows/index.js
@@ -19,6 +19,13 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>Shows</h1>
       <iframe
+        src="https://podcasters.spotify.com/pod/show/myworkman/embed/episodes/My-Work-Man-Show-Ep-IX-e206pvg"
+        height="120px"
+        width="500px"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+      <iframe
         src="https://anchor.fm/myworkman/embed/episodes/My-Work-Man-Show-Ep-VIII-e1vqp0t"
         height="120px"
         width="500px"
